212e2fdbfd640e7ea141151d1c37f4ae7d1c1b84
Your Name <holy@gmail.com>
2026-05-14 13:26:35 +0300

    Added working out conditions email in body

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Body  # Импортируем Path вместо Body
+from pydantic import EmailStr
 import uvicorn
 
 app = FastAPI()
 
 
-@app.get("/items")
+@app.get("/items/")
 def list_items():
     return [
         "Item1",
         "Item2",
         "Item3",
     ]
+
+
+@app.get("/items/{item_id}")
+def get_item_by_id(item_id: int = Path()):
+    return {
+        "message": "successful",
+        "id": item_id,
+    }
+
+@app.post("/users/")
+def create_user(email: EmailStr = Body()):
+    return {
+        "message": "success",
+        "email": email,
+
+    }
 
 
 if __name__ == "__main__":
