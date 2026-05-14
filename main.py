a353ea05e77f8d3a37c009170bca5abba9f84171
Matsoilan <holyloran@gmail.com>
2026-05-14 12:52:16 +0300

    First commit. Added first view, set settings

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/items")
+def list_items():
+    return [
+        "Item1",
+        "Item2",
+        "Item3",
+    ]
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
