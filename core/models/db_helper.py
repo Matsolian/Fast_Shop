e81419666fa49947949e718db135f37693c99a26
Minor <holyminor@gmail.com>
2026-05-16 22:32:56 +0300

    i don't undeastant it...

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,4 +1,10 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
 from core.config import settings
 
 
@@ -8,12 +14,23 @@ class DatabaseHelper:
             url=url,
             echo=settings.db_echo,
         )
-        self.session_maker = async_sessionmaker(
-            bind=self.engine,  # какой движок использовать
+        self.session_factory = async_sessionmaker(
             autoflush=False,  # не отправлять запросы автоматически
             autocommit=False,  # не сохранять изменения автоматически
             expire_on_commit=False,  # не сбрасывать объекты после commit
         )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.get_scoped_session() as session:
+            yield session
+            await session.remove()
 
 
 db_helper = DatabaseHelper(
