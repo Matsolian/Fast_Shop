8ae2336bfc4b9f30b5b041b8d6d69e025fbe0b7f
Minor <holyminor@gmail.com>
2026-05-16 20:50:15 +0300

    create absolute path

--- core/config.py
+++ core/config.py
@@ -1,8 +1,10 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
 
+BASE_DIR = Path(__file__).parent.parent
 
 class Setting(BaseSettings):
-    db_url: str = "sqlite+aiosqlite:///./db.sqlite3"
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
     # db_echo: bool = False
     db_echo: bool = (
         True  # когда мы работаем с отладкой, мы замедляем программу и небезопасно
