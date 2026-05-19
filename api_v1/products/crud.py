7b002a3fd29f81d710848df14de1eb54dfa84a2b
Minor <holyminor@gmail.com>
2026-05-19 17:05:09 +0300

    i did all crud

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import Product
-from .schemas import ProductCreate
+from .schemas import ProductCreate, ProductUpdate, ProductUpdatePartial
 
 
 async def get_products(session: AsyncSession) -> list[Product]:
@@ -32,3 +32,39 @@ async def create_product(session: AsyncSession, product_in: ProductCreate) -> Pr
 
 #  product_in.model_dump() превращает Pydantic-схему в словарь: {"name": "Apple", "price": 100}. Затем ** распаковывает его в аргументы конструктора
 #   SQLAlchemy-модели.
+
+
+async def update_product(
+    session: AsyncSession, product: Product, product_update: ProductUpdate
+) -> ProductUpdate:  # через put. ПОлностью заменяю все строки
+    for name, value in product_update.model_dump().items():
+        setattr(product, name, value)
+    await session.commit()
+    return product
+
+
+async def update_product_partial(
+    session: AsyncSession, product: Product, product_update: ProductUpdatePartial
+):  # Через Patch. Заменяю. лишь нужный атрибут
+    for name, value in product_update.model_dump(exclude_unset=True).items():
+        setattr(product, name, value)
+    await session.commit()
+    return product
+
+
+# Либо можещь объяединить, они ведь релаьно похожи... Но зато в partial можешь отедльно добавлять логику, если надо
+# async def update_product_partial_togetsher_mega_super(
+#     session: AsyncSession, product: Product, product_update: ProductUpdate | ProductUpdatePartial, partial = False
+# ) -> ProductUpdate:  # через put. ПОлностью заменяю все строки
+#     for name, value in product_update.model_dump(exclude_unset=partial).items():
+#         setattr(product, name, value)
+#     await session.commit()
+#     return product
+
+
+async def delete_product(
+    session: AsyncSession,
+    product: Product,
+) -> None:
+    await session.delete(product)
+    await session.commit()  #Необязательно, оно и так сохрпанится по умолчанию
--- api_v1/products/schemas.py
+++ api_v1/products/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, ConfigDict
 
 
-class ProductBase(BaseModel): #основа
+class ProductBase(BaseModel):  # основа
     name: str
     description: str
     price: int
@@ -11,7 +11,19 @@ class ProductCreate(ProductBase):  # чтобы создавать из JSON ф�
     pass
 
 
-class Product(ProductBase):  # выдавать данные, по умолчанию не JSON, а SQLAlchemy объект
-    model_config = ConfigDict(from_attributes=True)   # позволдяет принимать не только словари, но и объекты(благодаря флагу)
+class ProductUpdate(ProductCreate):  
+    pass
+
+class ProductUpdatePartial(ProductCreate):  
+    name: str | None = None
+    description: str | None = None
+    price: int | None = None
+
+
+class Product(
+    ProductBase
+):  # выдавать данные, по умолчанию не JSON, а SQLAlchemy объект
+    model_config = ConfigDict(
+        from_attributes=True
+    )  # позволдяет принимать не только словари, но и объекты(благодаря флагу)
     id: int
- 
