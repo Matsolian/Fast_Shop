815b80e732b1c5b83755809fee64a5b92d225020
maaric-linux <holyloran@gmail.com>
2026-05-21 21:22:53 +0300

    worked table

---
+++ core/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String(length=32), unique=True)
+    # Утвержадаем макисмальную длинну и  уникальность
+    description: Mapped[str]
+    phone: Mapped[int]
