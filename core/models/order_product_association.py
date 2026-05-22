d96c4b990f44497a371cc892e73785e677d5dfc4
maaric-linux <holyloran@gmail.com>
2026-05-22 22:28:41 +0300

    worked order_product_association table

---
+++ core/models/order_product_association.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Integer, Table, Column, ForeignKey, UniqueConstraint
+from .base import Base
+
+order_product_association_table = Table(
+    "order_product_association",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("order_id", ForeignKey("orders.id"), nullable=False, unique=True),
+    Column("product_id", ForeignKey("products.id"), nullable=False, unique=True),
+    UniqueConstraint("order_id", "product_id", name="idx_unique_order_product"),
+)
