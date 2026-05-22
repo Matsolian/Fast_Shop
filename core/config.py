559c564fabda49b7ce1211432bb6d5b19a6ecb65
maaric-linux <holyloran@gmail.com>
2026-05-22 11:56:57 +0300

    New class DBSettings

--- core/config.py
+++ core/config.py
@@ -1,16 +1,24 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
+DB_URL = BASE_DIR / "db.sqlite3"
 
 
-class Setting(BaseSettings):
-    api_v1_prefix: str = "/api/v1"
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+class DBSettings(BaseModel):
+    url: str = f"sqlite+aiosqlite:///{DB_URL}"
     # db_echo: bool = False
-    db_echo: bool = (
-        True  # когда мы работаем с отладкой, мы замедляем программу и небезопасно
+    echo: bool = (
+        True
+        # когда мы работаем с отладкой, мы замедляем программу и небезопасно
     )
 
 
-settings = Setting()
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+
+    db: DBSettings = DBSettings()
+
+
+settings = Settings()
