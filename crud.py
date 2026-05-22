f4932abff61b2624007c2de64d499ac61b7b4d66
maaric-linux <holyloran@gmail.com>
2026-05-22 12:24:31 +0300

    Make show_users_with_profile

--- crud.py
+++ crud.py
@@ -4,6 +4,7 @@ CRUD USERS
 
 import asyncio
 from sqlalchemy import Result, select
+from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper, User, Profile, Post
 
@@ -40,24 +41,40 @@ async def create_user_profile(
     return profile
 
 
+async def show_users_with_profile(
+    session: AsyncSession,
+) -> list[User]:
+    stmt = (
+        select(User).options(joinedload(User.profiles)).order_by(User.id)
+    )  # Из-за того, что это ассинхронный подход, то надо сразу подгрузить профили
+    # result: Result = await session.execute(stmt)
+    # users = result.scalars()
+    users = await session.scalars(stmt)
+    for user in users:
+        print(user)
+        if user.profiles:
+            print(user.profiles.first_name)
+        else:
+            print("User don't have profile")
+
 async def main():
     async with db_helper.session_factory() as session:
-        # await create_user(sesion=session, username="Bob")
-        # await create_user(sesion=session, username="Rock")
-        user_bob = await get_user_by_username(sesion=session, username="Bob")
-        # await get_user_by_username(sesion=session, username="John")
-        user_rock = await get_user_by_username(sesion=session, username="Rock")
-        await create_user_profile(
-            session=session,
-            user_id=user_rock.id,
-            first_name="Rock",
-            last_name="Senko",
-        )
-        await create_user_profile(
-            session=session,
-            user_id=user_bob.id,
-            first_name="Luci",
-        )
+        # # await create_user(sesion=session, username="Bob")
+        # # await create_user(sesion=session, username="Rock")
+        # user_bob = await get_user_by_username(sesion=session, username="Bob")
+        # user_john = await get_user_by_username(sesion=session, username="John")
+        # user_rock = await get_user_by_username(sesion=session, username="Rock")
+        # await create_user_profile(
+        #     session=session,
+        #     user_id=user_john.id,
+        #     first_name="John",
+        # )
+        # await create_user_profile(
+        #     session=session,
+        #     user_id=user_bob.id,
+        #     first_name="Luci",
+        # )
+        await show_users_with_profile(session=session)
 
 
 if __name__ == "__main__":
