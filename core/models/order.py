4504c782012550a2329ac8f9183991c87956d015
maaric-linux <holyloran@gmail.com>
2026-05-22 22:29:23 +0300

    link order, prodcut with order_product_association

--- core/models/order.py
+++ core/models/order.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 from datetime import datetime
+from .order_product_association import order_product_association_table
+
+if TYPE_CHECKING:
+    from .product import Product
 
 
 class Order(Base):
@@ -10,4 +17,8 @@ class Order(Base):
     created_data: Mapped[datetime] = mapped_column(
         server_default=func.now(),
         default=datetime.utcnow,
+    )
+    products: Mapped[list["Product"]] = relationship(
+        secondary=order_product_association_table,
+        back_populates="orders",
     )
--- core/models/product.py
+++ core/models/product.py
@@ -1,11 +1,23 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, relationship
 from .base import Base
+from .order_product_association import order_product_association_table
+
+if TYPE_CHECKING:
+    from .order import Order
+
 
 class Product(Base):
-    __tablename__ = "products"  # не обязательно теперь, мы в главном Base создали проперти
+    __tablename__ = (
+        "products"  # не обязательно теперь, мы в главном Base создали проперти
+    )
 
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
 
-
+    orders: Mapped[list["Order"]] = relationship(
+        secondary=order_product_association_table,
+        back_populates="products",
+    )
