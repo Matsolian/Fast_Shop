4e2764f2059e64e5381ae84fff516f4ef1db7d5c
maaric-linux <holyloran@gmail.com>
2026-05-30 19:52:52 +0300

    add some rgister with hash

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,6 +1,6 @@
 import secrets
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Header, status
 
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
@@ -29,14 +29,20 @@ usernames_to_password = {  # Так никогда не храним, это п�
     "admin2": "admin23",
 }
 
+static_auth_token_to_username = {  # Так никогда не храним, это простой пример
+    "brgfvedc": "admin",
+    "trwfgedwggyht6uj": "admin12",
+    "i7ujn6yh4bg5t3vrfecii7uh6": "admin23",
+}
 
-def get_auth_user_username(    # Проверка с админами
+
+def get_auth_user_username(  # Проверка с админами
     credantials: Annotated[
         HTTPBasicCredentials,
         Depends(security),
     ],
 ):
-    unauted_exc = HTTPException(  #Наша кастомная ошибка 
+    unauted_exc = HTTPException(  # Наша кастомная ошибка
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid username or password",
         headers={"WWW-Authenticate": "Basic"},
@@ -56,6 +62,24 @@ def get_auth_user_username(    # Проверка с админами
     return credantials.username
 
 
+def get_username_by_static_auth_token(
+    static_token: str = Header(alias="x-secret-auth-token"),
+) -> str:
+
+    if username := static_auth_token_to_username.get(static_token):  # тут мы прсото смотрим есть ли значение и сразу выдаем
+        return username
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="token invalid"
+    )
+
+    # if static_token not in static_auth_token_to_username:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_401_UNAUTHORIZED, detail="token invalid"
+    #     )
+    # return static_auth_token_to_username[static_token]  # тут мы еще проверку делаем 
+
+
 @router.get("/basic-auth_username/")
 def demo_basic_auth_username(
     auth_username: str = Depends(get_auth_user_username),
@@ -65,3 +89,16 @@ def demo_basic_auth_username(
         "message": "hello",
         "username": auth_username,
     }
+
+
+@router.get("/some-http-header-auth/")
+def demo_some_http_header(
+    auth_username: str = Depends(get_username_by_static_auth_token),
+):
+
+    return {
+        "message": "hello",
+        "username": auth_username,
+    }
+
+
