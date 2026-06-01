759ce1acd46768bd4769eead3372822b5a2f4391
maaric-linux <holyloran@gmail.com>
2026-06-01 10:05:01 +0300

    add schemas JWT  UserShema

--- user/schemas.py
+++ user/schemas.py
@@ -1,8 +1,18 @@
 from typing import Annotated
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 from annotated_types import MinLen, MaxLen
 
 
 class Create_User(BaseModel):
     username: Annotated[str, MinLen(3), MaxLen(20)]
     emaIL: EmailStr
+
+
+class UserShema(BaseModel):
+    model_config = ConfigDict(
+        strict=True
+    )  # чтобы была строгая типизация и  не перетакили типы из друг в друга
+    username: str
+    passwd: bytes
+    email: str | None = None
+    active: bool = True
