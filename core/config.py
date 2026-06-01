3d9adafca11daf0321029396da21448d752f6b42
maaric-linux <holyloran@gmail.com>
2026-06-01 10:04:40 +0300

    Worked encoded and decoded JWT

--- core/config.py
+++ core/config.py
@@ -15,10 +15,17 @@ class DBSettings(BaseModel):
     )
 
 
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+
+
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
     db: DBSettings = DBSettings()
+    auth_jwt: AuthJWT = AuthJWT()
 
 
 settings = Settings()
