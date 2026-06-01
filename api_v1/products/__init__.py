d9e0406bdad5a0a6c564be34798e00f80a9f528c
maaric-linux <holyloran@gmail.com>
2026-06-01 12:18:14 +0300

    create router  auth JWT

--- api_v1/products/__init__.py
+++ api_v1/products/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from api_v1.products.views import router as product_router
-
+from auth.demo_jwt_auth import router as auth_router_jwt
 router = APIRouter()
 router.include_router(router=product_router, prefix="/products")
+router.include_router(router=auth_router_jwt, prefix="/JWT")
